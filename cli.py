3f33c53b8aac289480b43753266d11b54c33b671
Cezary Heldt <cezary.heldt@gmail.com>
2018-10-25 11:46:09 +0200

    getting user input prototype

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-import accounts;
+import accounts, inspect;
 
 
 def account_login():
@@ -12,19 +12,58 @@ def account_login():
         print("Account: " + login + " does not exist.\n");
     else:
         if aclist.list[login] == password:
-            currAccount = accounts.Account(login);
+            accounts.currAccount = accounts.Account(login);
         else:
             print("Wrong password");
+
+def account_login_ac(login):
+    accounts.currAccount = accounts.Account(login);
+
+
+def account_create(login,password):
+    acfile = open("accounts.txt","a+");
+    acfile.write(login + "," + password);
+    account_login_ac(login);
+
+
+def account_logout():
+    accounts.currAccount = None;
+
+
+def show_user():
+    print(accounts.currAccount);
 
 
 def get_input():
     if accounts.currAccount is None:
-        pass;
+        command = input("/>").split(" ");
+        if command[0] not in commandListLO:
+            print(command[0] + " - command not fount.");
+            return;
+    else:
+        command = input("@"+accounts.currAccount.login+"/>").split(" ");
+        if command[0] not in commandListLI:
+            print(command[0] + " - command not fount.");
+            return;
+
+
+def execute_command(command):
+    get_input();
+    commandListLO(command[0])();
 
 commandListLO = {
     "login":account_login,
     "cruser":account_create,
-}
+};
+
+commandListLI = {
+    "logout":account_logout,
+    "current":show_user
+};
+
+
+if __name__ == "__main__":
+    execute_command();
 
 
 
